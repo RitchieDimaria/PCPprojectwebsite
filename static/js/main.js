92c75093a69824905da11cd3e235acd023b80a93
Ritchie Dimaria <ritchiedima@hotmail.com>
2022-12-23 17:39:39 -0500

    Initial commit. Contains basic layout of html, basic js and basic css. Also test.json file of how our backend code should output our generated dominos json file

---
+++ static/js/main.js
@@ -0,0 +1,81 @@
+document.addEventListener("DOMContentLoaded", () => {
+    createSquares();
+    createDominos();
+
+    const currOrder = [];
+    const goodOrder = ["0","1","3","3","2"]
+
+    const keys=document.querySelectorAll('.keyboard-container div button')
+
+    for(let i = 0;i<keys.length; i++) {
+        keys[i].onclick = ({target})=> {
+            const key = target.getAttribute("data-key");
+            if(key <5) {
+                addDomino(key);
+            }
+            else if(key == "verify")
+            {
+                verifySet();
+            }
+            
+            console.log(key);
+        }
+    }
+
+
+    function addDomino(key) {
+        const selDom = document.getElementById("iddom"+key);
+
+        const selSquare = document.getElementById("idsq"+currOrder.length);
+        let dom = document.createElement("button");
+        dom.setAttribute("class","domino-square");
+        dom.innerHTML = selDom.innerHTML
+
+        currOrder.push(key);
+        console.log(selDom);
+
+        selSquare.appendChild(dom);
+
+    }
+
+    function displayWin() {
+        const ele = document.getElementById("container");
+        let newele = document.createElement("h3");
+        newele.innerHTML ="YOU WIN";
+        ele.appendChild(newele);
+        ele.setAttribute("class","win");
+        console.log("won game");
+    }
+    
+    function verifySet() {
+        console.log(currOrder);
+        console.log(goodOrder);
+        if(JSON.stringify(currOrder)==JSON.stringify(goodOrder)) {
+            displayWin();
+        }
+    }
+
+
+    function createSquares() {
+        const gameBoard = document.getElementById("board")
+
+        for (let index = 0; index < 6; index++) {
+            let square = document.createElement("div");
+            square.classList.add("square");
+            square.setAttribute("id","idsq"+index);
+            gameBoard.appendChild(square);
+        }
+    }
+    function createDominos() {
+        const dominogrid = document.getElementById("dom-container")
+
+        for(let i = 0; i < 5; i ++) {
+            let setdomino = document.createElement("button");
+            setdomino.setAttribute("id","iddom"+i);
+            setdomino.setAttribute("data-key",i)
+            setdomino.innerHTML = "hehe <br> dom"+i
+            dominogrid.appendChild(setdomino);
+        }
+
+    }
+});
